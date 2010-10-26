38101336952c898bccb9e667583cabef55055160
Ren Yufei <renyufei83@yahoo.com.cn>
2010-10-26 15:36:16 -0400

    test built-in length property

--- MultiDimArrayDemo.java
+++ MultiDimArrayDemo.java
@@ -29,34 +29,12 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */ 
 
-class ArrayDemo {
-     public static void main(String[] args) {
-          int[] anArray;              // declares an array of integers
-
-          anArray = new int[10];      // allocates memory for 10 integers
-            
-          anArray[0] = 100; // initialize first element
-          anArray[1] = 200; // initialize second element
-          anArray[2] = 300; // etc.
-          anArray[3] = 400;
-          anArray[4] = 500;
-          anArray[5] = 600;
-          anArray[6] = 700;
-          anArray[7] = 800;
-          anArray[8] = 900;
-          anArray[9] = 1000;
-
-          System.out.println("Element at index 0: " + anArray[0]);
-          System.out.println("Element at index 1: " + anArray[1]);
-          System.out.println("Element at index 2: " + anArray[2]);
-          System.out.println("Element at index 3: " + anArray[3]);
-          System.out.println("Element at index 4: " + anArray[4]);
-          System.out.println("Element at index 5: " + anArray[5]);
-          System.out.println("Element at index 6: " + anArray[6]);
-          System.out.println("Element at index 7: " + anArray[7]);
-          System.out.println("Element at index 8: " + anArray[8]);
-          System.out.println("Element at index 9: " + anArray[9]);
-          
-          System.out.println(anArray.length);
+class MultiDimArrayDemo {
+                                                                                
+     public static void main(String[] args){
+          String[][] names = {{"Mr. ","Mrs. ","Ms. "},
+                               {"Smith","Jones"}};
+          System.out.println(names[0][0]+ names[1][0]); // Mr. Smith
+          System.out.println(names[0][2]+ names[1][1]); // Ms. Jones
      }
-} 
+}
